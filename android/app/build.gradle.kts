de01de367c44e588bc178210d2b5cafc66b129ea
LoveGPT <thelovegpt@gmail.com>
2025-05-22 12:35:07 -0700

    Fix: Set minSdk to 23 for Firebase compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,11 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.lovegpt_mobileapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23  // ✅ Required for firebase_auth 5.5.4+
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
